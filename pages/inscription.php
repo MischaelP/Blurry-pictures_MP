98dec2919e78b7e33d0f4659121740264c63c202
Laurefav <Laure.Favre28@laposte.net>
2023-06-05 12:31:17 +0200

    logo + css page de création de compte

--- pages/inscription.php
+++ pages/inscription.php
@@ -9,20 +9,23 @@
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <div class="max-w-xl">
-       <h1 class="title text-gray-900">Inscription</h1>
-
-        <form action="verif.php" method="POST" class="text-gray-500 flex flex-col m-5">
-
-            Prénom <input type="text" name="firstName" id="firstName" class="border-solid border-2 border-neutral-900 rounded pl-2" placeholder="firstname">
-            Nom <input type="text" name="name" id="name" class="border-solid border-2 border-neutral-900 rounded pl-2" placeholder="name">
-            Email <input type="email" name="email" id="email" class="border-solid border-2 border-neutral-900 rounded pl-2" placeholder="blabla@gmail.com">
-            Pseudo  <input type="text" name="pseudo" id="pseudo" class="border-solid border-2 border-neutral-900 rounded pl-2" placeholder="pseudo">
-            Mot de passe <input type="password" name="password" id="password" class="border-solid border-2 border-neutral-900 rounded pl-2" placeholder="xxxxxxxxx">
-            <input type="submit" value="Création" class="border-solid border-2 border-neutral-900 bg-slate-300 m-2 rounded">
-        </form>
-    </div>
     
+   
+       <div class="box"> 
+        
+            <div class="formulaire">
+            <img id="logo" src="..\img\logo.png" alt="logo">
+        <form action="verif.php" method="POST" class="form text-gray-500 flex flex-col m-5">
+
+            Prénom <input type="text" name="firstName" id="firstName" class="input border-solid border-2 border-stone-400 rounded pl-2" placeholder="firstname">
+            Nom <input type="text" name="name" id="name" class="input border-solid border-2 border-stone-400 rounded pl-2" placeholder="name">
+            Email <input type="email" name="email" id="email" class="input border-solid border-2 border-stone-400 rounded pl-2" placeholder="blabla@gmail.com">
+            Pseudo  <input type="text" name="pseudo" id="pseudo" class="input border-solid border-2 border-stone-400 rounded pl-2" placeholder="pseudo">
+            Mot de passe <input type="password" name="password" id="input password" class="border-solid border-2 border-stone-400 rounded pl-2" placeholder="xxxxxxxxx">
+           <button type="submit" value="Création" class="button rounded-full border-solid border-2 border-transparent bg-slate-300 m-2 rounded"> Création </button>
+        </form>
+            </div>
+      </div>
     
 </body>
 </html>
